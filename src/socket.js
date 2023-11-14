fb1301a028e9d7659473229d526bf9c139841109
Marian Fedkovich <marianfedkovich@gmail.com>
2023-11-14 18:28:13 +0200

    x

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
-import * as io from 'socket.io-client'
+import socketio from "socket.io-client/dist/socket.io";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.REACT_APP_ENVIRONMENT === 'production' ? 'https://server.illusiumgame.com' : 'http://localhost:9000';
 
-export const socket = io(URL);
+export const socket = socketio(URL);
